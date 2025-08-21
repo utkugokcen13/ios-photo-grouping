b2d2f3d496ce8d1729d1ccde8d9cf487fc94f014
Utku Gökçen <utku.gokcen13@gmail.com>
2025-08-21 16:25:10 +0300

    Initial Commit

---
+++ PhotoGrouping/PhotoGroupingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGroupingApp.swift
+//  PhotoGrouping
+//
+//  Created by Utku Gökçen on 21.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGroupingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
